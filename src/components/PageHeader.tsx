27bbeaaf7b846ecab4d2477fcad0ba7f6c893691
jmedina128 <medinajesus821@gmail.com>
2024-06-07 21:15:36 -0400

    header

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Flex, HStack, Img, Spacer, Stack, Text } from "@chakra-ui/react";
+import Logo from '../assets/logo-meru.png';
+import useSession from "../hooks/useSession";
+
+export default function PageHeader() {
+
+    const { user } = useSession()
+
+    return <Flex marginBottom='40px' bg='blue.900' alignItems={'center'} paddingX={'2.5%'} gap={'2ch'}>
+        <Img h='64px' src={Logo} alt='Logo Meru' />
+        <Stack spacing={1}>
+            <Text
+                color='white'
+                margin={0}
+                fontWeight={'bold'}
+                fontSize='xxx-large'>
+                Alimentos Meru
+            </Text>
+            <Text fontWeight={'bold'} color='white'>
+                Cobranzas
+            </Text>
+        </Stack>
+        <Spacer />
+        <HStack spacing={2}>
+            <Text color='white'>
+                {user?.name}
+            </Text>
+            <Avatar size='md' name={user?.name} />
+        </HStack>
+    </Flex>
+}
