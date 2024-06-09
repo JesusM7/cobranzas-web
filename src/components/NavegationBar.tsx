bf2450b49ec1083729e7c313276c4c392e840727
JesusG <medinajesusg821@gmail.com>
2024-06-09 11:15:57 -0400

    NavegationBar modify

--- src/components/NavegationBar.tsx
+++ src/components/NavegationBar.tsx
@@ -2,13 +2,13 @@ import { Button, ButtonGroup } from "@chakra-ui/react";
 
 export default function NavegationBar() {
 
- return <ButtonGroup variant='outline' spacing='6' size={"lg"} border={"solid red"} paddingLeft={9}  >
-<Button colorScheme='blue'>Pagos</Button>
-<Button colorScheme='blue'>Cuentas</Button>
-<Button colorScheme='blue'>Clientes</Button>
-<Button colorScheme='blue'>Vendedores</Button>
-<Button colorScheme='blue'>Productos</Button>
-<Button colorScheme='blue'>Configuracion</Button>
+ return <ButtonGroup variant='outline' spacing='6' size={"lg"} paddingLeft={9}>
+<Button variant={"link"}>Pagos</Button>
+<Button variant={"link"}>Cuentas</Button>
+<Button variant={"link"}>Clientes</Button>
+<Button variant={"link"}>Vendedores</Button>
+<Button variant={"link"}>Productos</Button>
+<Button variant={"link"}>Configuracion</Button>
 </ButtonGroup>
 
 }
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,10 +1,11 @@
-import {Flex, Img, Spacer, Stack, Text } from "@chakra-ui/react";
+import { Flex, Img, Spacer, Stack, Text } from "@chakra-ui/react";
 import Logo from '../assets/logo-meru.png';
 import UserMenu from "./UserMenu";
+import NavegationBar from "./NavegationBar";
 
 export default function PageHeader() {
 
-    return <Flex marginBottom='40px' bg='blue.900' alignItems={'center'} paddingX={'2.5%'} gap={'2ch'}>
+    return <Stack marginBottom='40px' bg='blue.900' ><Flex alignItems={'center'} paddingX={'2.5%'} gap={'2ch'}>
         <Img h='64px' src={Logo} alt='Logo Meru' />
         <Stack spacing={1}>
             <Text
@@ -21,4 +22,6 @@ export default function PageHeader() {
         <Spacer />
         <UserMenu />
     </Flex>
+        <NavegationBar />
+    </Stack>
 }
