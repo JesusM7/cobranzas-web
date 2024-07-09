c1408ce16fcb4fe8bed8b2d850d3b9319451886f
jmedina128 <medinajesus821@gmail.com>
2024-07-08 22:32:41 -0400

    fxed

--- src/components/navigator/DocumentsMenu.tsx
+++ src/components/navigator/DocumentsMenu.tsx
@@ -12,7 +12,6 @@ export default function DocumentsMenu() {
         </MenuButton>
         <MenuList>
             <MenuItem to={"/facturas"} as={Link}> Facturas </MenuItem>
-            <MenuItem> Notas de Debito </MenuItem>
         </MenuList>
     </Menu>
 }
